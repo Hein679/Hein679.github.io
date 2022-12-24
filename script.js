f253c5bb5d4d53043956b67a69e92b958771666e
Hein Htet Win <heinhtetw679@gmail.com>
2022-12-24 22:41:29 +0000

    Ocean HTML/CSS/JavaScript, First stage commit.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 async function getQuote() {
   const response = await fetch(
-    "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
+    "https://cors-anywhere.herokuapp.com/https://zenquotes.io/api/random"
+    // https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en (This API generating CORS error)
   );
   const data = await response.json();
+  console.log(data);
   return data;
 }
 
@@ -10,7 +12,7 @@ const quoteElement = document.getElementById("quote");
 
 async function displayQuote() {
   const quote = await getQuote();
-  quoteElement.innerText = `"${quote.quoteText}" - ${quote.quoteAuthor}`;
+  quoteElement.innerText = `"${quote.q}" - ${quote.a}`;
 }
 
 displayQuote();
